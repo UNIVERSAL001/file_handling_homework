f38d6a05a86553d68bcc23d7d580d1bca2559ef4
Murodjon <developer1982002@gmai.com>
2023-01-17 10:52:49 +0500

    05

--- file05.py
+++ file05.py
@@ -6,5 +6,14 @@ def main(data:str):
     Returns:
         list: return answer
     """
-    
+    number = 0
+
+    for i in data:
+        number+=i.isdigit()
+
+
+    return [number,len(data)-number]
 # Read data from file
+path = "txt_file/data05.txt"
+f = open(path).read()
+print(main(f))
