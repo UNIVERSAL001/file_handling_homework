eff126ba4a16de965fd70028fc728fdc03bbabb3
Murodjon <developer1982002@gmai.com>
2023-01-17 10:41:03 +0500

    01

--- file01.py
+++ file01.py
@@ -6,5 +6,11 @@ def main(data:str):
     Returns:
         list: return answer
     """
-
+    s = []
+    for i in data.split(","):
+        s.append(int(i))
+    return s
 # Read data from file
+path = "txt_file/data01.txt"
+f = open(path).read()
+print(main(f))
